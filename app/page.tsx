c61d5e885270ddedabf3f27302f65ae4115cc5c6
DArlegui <79351401+DArlegui@users.noreply.github.com>
2024-08-03 09:03:48 +0900

    78 small bar userinfo (#83)
    * Added NameForm.tsx for users name validation if they don't have one already + route.ts for the update API functionality, UserProfileFooter.tsx which shows the user's profile picture and name.
    
    * chore: Fixed the MessageLog.tsx container. Refactor CSS styles and remove unnecessary margins and padding.

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,69 @@
-'use client'
-
-import React, { useState } from "react";
+"use client";
+import { currentUser } from "@/lib/current-user";
+import { User } from "@prisma/client";
+import { useEffect, useState } from "react";
 import MessageLog from "./MessageLog";
+import NameForm from "./NameForm";
+import UserProfileFooter from "./UserProfileFooter";
+import ChannelList from "./components/ChannelList/ChannelList";
 import SideBar from "./components/SideBar/SideBar/SideBar";
 import UserList from "./components/dm-list/dm-list";
-import ChannelList from "./components/ChannelList/ChannelList";
 
 export default function Home() {
   const [selectedServerId, setSelectedServerId] = useState<string | null>(null);
   const [selectedChannelName, setSelectedChannelName] = useState<string>("");
   const [selectedChannelId, setSelectedChannelId] = useState<string>("");
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user: User | null = await currentUser();
+      if (user) setUser(user);
+    };
+
+    fetchUser();
+  }, []);
+
+  if (!user) return null;
 
   return (
-    <div className="project-container">
-      <div className="main-rows bg-white">
-        <div className="side-channels-container">
-          <SideBar onSelectServer={setSelectedServerId} />
-        </div>
-        <div className="messages-bar">
-          <div className="w-[360px] h-[74px] left-0 top-0 absolute border-opacity-25"></div>
-        </div>
-        <div className="direct-messages-container">
-          {selectedServerId ? (
-            <ChannelList serverId={selectedServerId} 
-            onChannelSelect={(channelId, channelName) => {
-              setSelectedChannelId(channelId);
-              setSelectedChannelName(channelName);
-            }}/>
-          ) : (
-            <UserList />
-          )}
-        </div>
-        <div className="message-log-container">
-          <MessageLog channelName={selectedChannelName} channelId={selectedChannelId} />
+    <div className="relative h-screen overflow-hidden">
+      {!user.name && <NameForm setUser={setUser} user={user} />}
+      <div className="project-container h-full">
+        <div className="main-rows bg-white h-full">
+          {/* Side Channels Container */}
+          <div className="side-channels-container">
+            <SideBar onSelectServer={setSelectedServerId} />
+          </div>
+
+          {/* Messages Bar */}
+          <div className="messages-bar">
+            <div className="absolute left-0 top-0 w-[360px] h-[74px] border-opacity-25"></div>
+          </div>
+
+          {/* Direct Messages Container */}
+          <div className="relative direct-messages-container">
+            {selectedServerId ? (
+              <ChannelList
+                serverId={selectedServerId}
+                onChannelSelect={(channelId, channelName) => {
+                  setSelectedChannelId(channelId);
+                  setSelectedChannelName(channelName);
+                }}
+              />
+            ) : (
+              <UserList />
+            )}
+            <UserProfileFooter user={user} />
+          </div>
+
+          {/* Message Log Container */}
+          <div className="message-log-container">
+            <MessageLog
+              channelName={selectedChannelName}
+              channelId={selectedChannelId}
+            />
+          </div>
         </div>
       </div>
     </div>
